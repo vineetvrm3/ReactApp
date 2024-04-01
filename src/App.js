b7f74ebfb203f9c20d115a37df8ed772692b57ad
vineet verma <vineetvrm3@gmail.com>
2024-04-02 00:20:17 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Testing React App by Vineet verma</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
     </div>
   );
